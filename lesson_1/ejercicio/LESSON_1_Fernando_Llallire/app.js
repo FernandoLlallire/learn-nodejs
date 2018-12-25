5de948d8177018d65a2f67a7768c8079bb5bde97
fernando.llallire <fernandorllallire@gmail.com>
2018-12-25 11:16:35 -0300

    intermediate state

--- lesson_1/ejercicio/LESSON_1_Fernando_Llallire/app.js
+++ lesson_1/ejercicio/LESSON_1_Fernando_Llallire/app.js
@@ -1,4 +1,12 @@
 const argv = require('yargs').argv;
-const file =  require('file');
+const file =  require('./File.js');
+const msgs = require('./Msgs.js');
 // ejecucion mediante ./app.js --msg="Demo MSG"
 const msg = argv.msg;
+const messages = msgs.msgFactory();
+const logFile = file.fileFactory("config.json");
+messages.concatenate(msg);
+messages.concatenate(new Date().toString());
+logFile.obtainLogFileName();
+logFile.setMessage(messages.messenger());
+logFile.updateFile();
--- lesson_1/ejercicio/LESSON_1_Fernando_Llallire/file.js
+++ lesson_1/ejercicio/LESSON_1_Fernando_Llallire/file.js
@@ -1,28 +1,84 @@
 const fs = require('fs');
 
-const  readPromise = (fileName) =>
+const  openPromise = (proto) =>
   new Promise((resolve, reject) => {
-    fs.readFile(fileName,'utf8',(err,data) => {
-      if(err) return reject(fileName);
-      return resolve(fileName);
+    fs.readFile(proto.logFile,'utf8',(err,data) => {
+      if(err) return reject(proto);
+      return resolve(proto);
     })
   })
 
-const createPromise = (fileName, msg = '') =>
+const createPromise = (proto) =>
   new Promise ((resolve,reject) => {
-    fs.writeFile(fileName, msg, 'utf8', (err) => {
+    fs.writeFile(proto.logFile, '', 'utf8', (err) => {
       if(err) return reject (err);
-      return resolve(fileName);
+      return resolve(proto);
     })
   })
 
-const appendPromise = (fileName, msg = '') => new Promise((resolve, reject) => {
-  fs.appendFile(fileName, msg, 'utf8', (err) => {
+const appendPromise = (proto) =>
+ new Promise((resolve, reject) => {
+  fs.appendFile(proto.logFile, proto.msg, 'utf8', (err) => {
     if (err) return reject(err);//Le dejo el reject sin asignar por si quiero asignarle alguna tarea con algun catch.
     return resolve("File Update");
   })
 })
+const  readPromise = (proto) =>
+  new Promise((resolve, reject) => {
+    fs.readFile(proto.jsonFileName,'utf8',(err,data) => {
+      if(err) return reject(err);
+      return resolve(data);
+    })
+  })
+/******************************************************************************/
+const obtainLogFileName = (proto) =>({
+  obtainLogFileName : () => readPromise(proto)
+  .then((jsonData) => {proto.logFile = JSON.parse(jsonData).nombre.toString();console.log(proto.logFile)})
+  .catch((err) => console.log(err))
+})
 
-readPromise("log.html")
-.catch((fileName) => createPromise(fileName))
-.then((fileName) => appendPromise(fileName,"texto"));
+const updateFile = (proto) =>({
+  updateFile : () => openPromise(proto)
+  .catch((data)=>createPromise(data))
+  .then((data)=>appendPromise(data))
+})
+
+const setJsonFile = (proto) => ({
+  setJsonFile : (fileName) => {proto.jsonFileName = fileName}
+})
+
+const setMessage = (proto) => ({
+  setMessage : (message) => {proto.msg = message}
+})
+
+const readFileName = (proto) => ({
+  readFileName : (jsonFile) => {
+    proto.jsonfile = jsonFile;
+    openPromise(proto)
+  }
+})
+
+const fileFactory = (jsonFileName) => {
+  const proto = {
+    jsonFileName
+  }
+  // obtainLogFileName(proto).obtainLogFileName();
+  return Object.assign(
+    {},
+    updateFile(proto),
+    setJsonFile(proto),
+    setMessage(proto),
+    obtainLogFileName(proto)
+  )
+}
+exports.fileFactory = fileFactory;
+// exports.fileObject =  (fileName, msg) => {
+//   const proto = {
+//     fileName,
+//     msg
+//   }
+//   return Object.assign(
+//     {},
+//     updateFile(proto)
+//   )
+// };
