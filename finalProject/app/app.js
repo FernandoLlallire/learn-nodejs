1e9815038d8faf6bb2941ae282d72e6884a89e18
Fernando Raul Llallire <fernando.llallire@globant.com>
2019-01-22 15:51:17 -0300

    swagger 2.0 has problem with cookie

--- finalProject/app/app.js
+++ finalProject/app/app.js
@@ -5,12 +5,26 @@ const bodyParser = require('body-parser');
 const user = require('./routes/users');
 const video = require('./routes/videos');
 const cookieParser = require('cookie-parser');
+const swaggerUi = require('swagger-ui-express');
+/*
+Hay que tener cuidado al pasar de yaml a json necesitamos algo que convierta los formatos.
+Yaml es mejor para trabajar por que podemos leerlo mas facil 
+https://www.npmjs.com/package/swagger-ui-express
+Ademas necesitamos pasarle todo el path al YAML.load 
+https://stackoverflow.com/questions/50105334/cant-read-a-file-inside-functions-folder
+*/
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load(path.resolve(__dirname,'./api/swagger/swagger.yaml'));
+
+
+
 
 const app = express();
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/public', express.static(__dirname+'/public'));
-app.use(bodyParser.json());//Para poder obtener los datos del body
-app.use(bodyParser.urlencoded());//para el parseo
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use('/video', video);
 app.use('/', user);
