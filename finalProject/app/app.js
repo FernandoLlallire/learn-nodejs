9209d62b4fd0433035f13b732c24be8e0c6639a3
fernando <fernando@localhost.localdomain>
2019-01-23 02:06:05 -0300

    swagger ok

--- finalProject/app/app.js
+++ finalProject/app/app.js
@@ -8,24 +8,26 @@ const cookieParser = require('cookie-parser');
 const swaggerUi = require('swagger-ui-express');
 /*
 Hay que tener cuidado al pasar de yaml a json necesitamos algo que convierta los formatos.
-Yaml es mejor para trabajar por que podemos leerlo mas facil 
+Yaml es mejor para trabajar por que podemos leerlo mas facil
 https://www.npmjs.com/package/swagger-ui-express
-Ademas necesitamos pasarle todo el path al YAML.load 
+Ademas necesitamos pasarle todo el path al YAML.load
 https://stackoverflow.com/questions/50105334/cant-read-a-file-inside-functions-folder
 */
 const YAML = require('yamljs');
 const swaggerDocument = YAML.load(path.resolve(__dirname,'./api/swagger/swagger.yaml'));
-
+const options = {
+  explorer : true
+};
 
 
 
 const app = express();
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.use('/public', express.static(__dirname+'/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use('/video', video);
 app.use('/', user);
+app.use('/public', express.static(__dirname+'/public'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 module.exports = app;
